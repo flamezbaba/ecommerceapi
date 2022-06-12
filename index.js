8c566159c996b7aeb97148f87ebafcda028cd472
Sangosanya Segun <sangosanyasegun@yahoo.com>
2022-06-12 06:16:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ dotenv.config();
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/products");
+const orderRoute = require("./routes/orders");
 
 if (process.env.NODE_ENV === "test") {
   var DBUrl = process.env.MONGO_TEST_URL;
@@ -27,6 +28,7 @@ app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products/", productRoute);
+app.use("/api/orders/", orderRoute);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => `Backend Server running on port ${port} 🔥`);
